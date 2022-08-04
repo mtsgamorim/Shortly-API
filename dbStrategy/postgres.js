63e851c7911116e24a0ce983d35fa592968fc5fd
Matheus Amorim <mtsgamorim@gmail.com>
2022-08-04 17:13:52 -0300

    prepare for deploy

--- dbStrategy/postgres.js
+++ dbStrategy/postgres.js
@@ -1,12 +1,16 @@
-import dotenv from "dotenv";
 import pg from "pg";
-
-dotenv.config();
+import dotenv from "dotenv";
 
 const { Pool } = pg;
+dotenv.config();
 
-const connection = new Pool({
+const databaseConfig = {
   connectionString: process.env.DATABASE_URL,
-});
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
+
+const connection = new Pool(databaseConfig);
 
 export default connection;
